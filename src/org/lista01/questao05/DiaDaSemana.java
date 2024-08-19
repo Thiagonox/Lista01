228e2c1d831381a0644b2526fd76a1d6b15653e7
thiagonox <thiagogentil499@gmail.com>
2024-08-19 19:33:29 -0300

    Fix: Atendendo a requisito da questão

--- src/org/lista01/questao05/DiaDaSemana.java
+++ src/org/lista01/questao05/DiaDaSemana.java
@@ -3,13 +3,13 @@ package org.lista01.questao05;
 public class DiaDaSemana {
     public static String Converte (int dia) {
         String[] diasDaSemana = {
-                "Domingo",
                 "Segunda-feira",
                 "Terça-feira",
                 "Quarta-feira",
                 "Quinta-feira",
                 "Sexta-feira",
-                "Sábado"
+                "Sábado",
+                "Domingo"
         };
         if (dia <= 7 && dia >= 1) {
             return diasDaSemana[dia - 1];
